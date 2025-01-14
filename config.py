bccf474a0266308b58c35d08085da9c1ea8d288b
4-0-4 <146018190+nyeinkokoaung404@users.noreply.github.com>
2025-01-15 02:04:17 +0900

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 # Channel: https://t.me/premium_channel_404
 
 # Pyrogram setup
-BOT_TOKEN = "7500510776:AAFgvxpbsbuwdIm-K7w7nPKddPNaMNGMugc"  # Replace this BOT_TOKEN
+BOT_TOKEN = "7922669430:AAER8KHpHHropOW-HMA6X_S4-HimsolpQ4A"  # Replace this BOT_TOKEN
 
 # Admin ID
 ADMIN_ID = "1273841502"  # Replace this ADMIN_ID
